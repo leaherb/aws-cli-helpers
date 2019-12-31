7662df17d086ee5f26ba6face822e8214fff7be7
leaherb <leah@erbinweb.com>
2019-12-31 07:10:52 -0800

    docs: Add comment

--- show_keys.sh
+++ show_keys.sh
@@ -1,3 +1,4 @@
+# List key pairs used by EC2 instances. 
 aws ec2 describe-instances \
   --query 'Reservations[*].Instances[*].[InstanceId, KeyName,State.Name]' \
   --filters Name=instance-state-name \
